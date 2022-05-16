cbe83f806d1467c81f5123d7627685b26b253033
Noam Honig <noam.honig@gmail.com>
2022-05-16 15:56:07 +0300

    start playing with the html

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,3 +1,18 @@
 import { remultExpress } from 'remult/remult-express';
+import { Task } from '../shared/Task';
 
-export const api = remultExpress();
+export const api = remultExpress({
+    entities: [Task],
+    initApi: async remult => {
+        const taskRepo = remult.repo(Task);
+        if (await taskRepo.count() === 0) {
+            await taskRepo.insert([
+                { title: "Task a" },
+                { title: "Task b", completed: true },
+                { title: "Task c" },
+                { title: "Task d" },
+                { title: "Task e", completed: true }
+            ]);
+        }
+    }
+});
